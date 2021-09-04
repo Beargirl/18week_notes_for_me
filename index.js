730850936de3ef58c9acfd5dc38013b7f42c20cc
Beargirl <cool.beargirl@yandex.ru>
2021-09-04 22:42:10 +0300

    -

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function saveNote() {
     let name = document.getElementById("name").value;
     let text = document.getElementById ("note_text").value;
 
-    localStorage.setItem("dayMonthYear", date.value);
-    localStorage.setItem("noteName", name.value);
-    localStorage.setItem("noteAbout", text.value);
+    localStorage.setItem("dayMonthYear", "date");
+    localStorage.setItem("noteName", "name");
+    localStorage.setItem("noteAbout", "text");
 
     if(document.getElementById ("note_text").value === '' || document.getElementById ("name").value === '' || document.getElementById("date").value === '') {
         alert('Не все поля заполнены');
